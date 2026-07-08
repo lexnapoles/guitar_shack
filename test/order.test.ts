666cadb3339ba95123a2705f0c3153e68f59ed34
Alejandro Napoles <14150661+lexnapoles@users.noreply.github.com>
2026-07-08 16:11:00 +0100

    Move order outside of test folder

--- test/order.test.ts
+++ test/order.test.ts
@@ -1,7 +1,7 @@
 import "mocha"
 import { strict as assert } from "assert";
 import {describe, it} from "node:test";
-import { Order } from "./order";
+import { Order } from "../src/order";
 
 describe("Add item to order", () => {
     it("should add an item when sufficient stock is available", () => {
